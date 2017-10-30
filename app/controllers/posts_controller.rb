f2c0e2a09de948602a1d051b44c27e0d74b455f5
r-mor <morales.randl@gmail.com>
2017-10-30 14:48:05 -0400

    added form partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@posts = Post.all
@@ -23,6 +23,12 @@ class PostsController < ApplicationController
 	def update
 		@post.update(post_params)
 		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@post.destroy
+
+		redirect_to posts_path
 	end
 
 
