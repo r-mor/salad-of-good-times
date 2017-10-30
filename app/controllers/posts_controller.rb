040bb750a157f4ae1e617f4021dca67cf72b7b8c
r-mor <morales.randl@gmail.com>
2017-10-30 14:30:37 -0400

    added edit action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
 
 	def index
 		@posts = Post.all
@@ -12,10 +12,20 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def edit
+	end
+
 	def create
 		@post = Post.create(post_params)
 		redirect_to posts_path
 	end
+
+	def update
+		@post.update(post_params)
+		redirect_to post_path(@post)
+	end
+
+
 
 private
 
